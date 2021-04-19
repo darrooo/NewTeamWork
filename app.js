7c32b0f658ca768fe4e84c358c78efece9e38f56
AgnesOlofsson <Agnes.Olofsson.9162@student.uu.se>
2021-04-19 14:49:22 +0200

    grund till chatt skapad

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+
+
 const express = require('express');
 const app = express();
 var http = require('http').Server(app);
 var path = require('path');
+const io = require('socket.io')(http);
 const port = 3000;
+
+
+//Agnes testar sockets här
+//const express = require('express');
+//const app = express();
+//const http = require('http');
+//const server = http.createServer(app);
+//const port = 3000;
+// till hit
+
 
 //app.get('/', (req, res) => {
 //  res.send('Hello World!')
@@ -14,19 +27,37 @@ app.use(express.static(path.join(__dirname, 'public/')));
 //  res.sendFile(path.join(__dirname, 'views/teamworkpage.html'));
 //});
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'public/views/index.html'));
+ });
+
+ app.get('/homepage', function (req, res) {
+   res.sendFile(path.join(__dirname, 'public/views/homepage.html'));
+ });
+
+ app.get('/about', function (req, res) {
+   res.sendFile(path.join(__dirname, 'public/views/about.html'));
 });
 
-app.get('/homepage', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public/views/homepage.html'));
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/public/views/chat.html');
 });
 
-app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public/views/about.html'));
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
+
+io.on('connection', (socket) => {
+  //console.log('a user connected');
+  //socket.on('disconnect', () => {
+  //console.log('user disconnected');
+  //  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);   //Här vill vi lägga in typ "[userName]: + msg"
+    console.log('message: ' + msg);
+
+  });
 });
 
-
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
