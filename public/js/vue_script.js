cd3080e707acb0d6de29e33f7d1ffe7495ef695f
AgnesOlofsson <Agnes.Olofsson.9162@student.uu.se>
2021-05-18 12:24:55 +0200

    knappfix och färgändring

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -50,5 +50,14 @@ function(d){
     if(window.location == "http://localhost:3000/homepage" && access ==false){
       window.location = "http://localhost:3000/";
     }
-  
+    else if(window.location == "http://localhost:3000/myProfile" && access ==false){
+      window.location = "http://localhost:3000/";
+    }
+    else if(window.location == "http://localhost:3000/settings" && access ==false){
+      window.location = "http://localhost:3000/";
+    }
+    else if(window.location == "http://localhost:3000/chat" && access ==false){
+      window.location = "http://localhost:3000/";
+    }
+
   });
